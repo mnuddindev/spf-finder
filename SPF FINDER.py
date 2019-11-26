cd46741ade69745fee029a95a250fe4d0c0aadce
inadislam <48486098+inadislam@users.noreply.github.com>
2019-11-26 15:36:52 +0600

    Dona

---
+++ SPF FINDER.py
@@ -0,0 +1,35 @@
+import requests as r
+from bs4 import BeautifulSoup
+
+a = open("list.txt").read().split('\n')
+b = open("result.txt", "a+")
+
+print("Script Started Successfully")
+
+for domain in a:
+    if (domain == ""):
+        continue
+    headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+            }
+
+    url = "https://www.kitterman.com/spf/getspf2.py"
+
+    values = {
+            "serial": "fred12",
+            "domain": domain
+            }
+
+    p = r.post(url, data=values, headers=headers)
+
+    html = p.text
+
+    print("Checking for ===> "+domain)
+
+    if ("No valid" in html):
+        print("SPF not Found For ===> "+domain)
+        b.write(domain+'\n')
+    else:
+        print("SPF Record Are published For: "+domain)
+
+print("Script Finished Thank You")
